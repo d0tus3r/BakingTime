3b9c1ca642a9dd0dcd0d9bb2993520d8d5897702
d0tus3r <d0tus3r@gmail.com>
2018-07-27 12:39:20 -0400

    initial work on fragment

--- app/src/main/java/net/digitalswarm/bakingtime/RecipeDetailActivity.java
+++ app/src/main/java/net/digitalswarm/bakingtime/RecipeDetailActivity.java
@@ -1,4 +1,29 @@
 package net.digitalswarm.bakingtime;
 
-public class RecipeDetailActivity {
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import net.digitalswarm.bakingtime.fragments.RecipeMasterDetailFragment;
+import net.digitalswarm.bakingtime.models.Recipe;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+
+    Recipe currentRecipe;
+    //Todo: change title bar to recipe
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recipe_detail_activity);
+        //assign recipe from intent
+        this.currentRecipe = getIntent().getParcelableExtra("recipe");
+        //start ingredients and step list fragments
+        //attach within frame layout on detail_frame
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.detail_frame, RecipeMasterDetailFragment.newInstance(currentRecipe.getIngredients(), currentRecipe.getRecipeSteps()))
+                .commit();
+    }
+    //Todo: add onClick methods for recipe step clicks
+
 }
---
+++ app/src/main/java/net/digitalswarm/bakingtime/fragments/RecipeMasterDetailFragment.java
@@ -0,0 +1,9 @@
+package net.digitalswarm.bakingtime.fragments;
+
+import android.support.v4.app.Fragment;
+
+public class RecipeMasterDetailFragment extends Fragment {
+
+
+
+}
