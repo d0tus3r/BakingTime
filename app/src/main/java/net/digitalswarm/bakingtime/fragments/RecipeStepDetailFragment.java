59eec62ae79a91532db18272fb62074df3222644
Andrew Deese <d0tus3r@gmail.com>
2018-08-02 23:20:17 -0400

    exoplayer working
    needs style work, widget finished, expresso testing

--- app/src/main/java/net/digitalswarm/bakingtime/fragments/RecipeStepDetailFragment.java
+++ app/src/main/java/net/digitalswarm/bakingtime/fragments/RecipeStepDetailFragment.java
@@ -1,24 +1,39 @@
 package net.digitalswarm.bakingtime.fragments;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 
 import net.digitalswarm.bakingtime.R;
 import net.digitalswarm.bakingtime.models.RecipeSteps;
 
-public class RecipeStepDetailFragment extends Fragment {
+public class RecipeStepDetailFragment extends Fragment implements Player.EventListener {
 
     //keys for changing steps
     private static final String STEP_ID_KEY = "id";
@@ -32,10 +47,9 @@ public class RecipeStepDetailFragment extends Fragment {
 
     private RecipeSteps currentStep;
     private int currentStepId;
-    private FragmentActivity fragmentContext;
-
+    private String currentStepVideoUrl;
     private SimpleExoPlayer mExoPlayer;
-    private SimpleExoPlayerView mPlayerView;
+    //private SimpleExoPlayerView mPlayerView;
     private static MediaSessionCompat mMediaSession;
     private PlaybackStateCompat.Builder mStateBuilder;
     OnPrevStepsClickListener mPrevCallback;
@@ -81,6 +95,23 @@ public class RecipeStepDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //inflate layout
         View rootView = inflater.inflate(R.layout.recipe_step_detail_fragment, container, false);
+        //exoplayer
+        SimpleExoPlayerView mPlayerView = rootView.findViewById(R.id.recipe_step_exo_view);
+        currentStepVideoUrl = currentStep.getVideoUrl();
+        Log.d(TAG, "videoUrl = " + currentStepVideoUrl);
+
+        if (savedInstanceState != null) {
+            mExoPosition = savedInstanceState.getLong(EXO_POS_KEY);
+            mExoState = savedInstanceState.getBoolean(EXO_STATE_KEY);
+            mExoFullscreen = savedInstanceState.getBoolean(EXO_FULLSCREEN_KEY);
+        }
+        //hide view if no videoUrl
+        if (currentStepVideoUrl.isEmpty()) {
+            mPlayerView.setVisibility(View.GONE);
+        } else {
+            initMediaSession();
+            initExoPlayer(Uri.parse(currentStepVideoUrl), mPlayerView);
+        }
         //assign text view and set text
         TextView stepDescriptionTV = rootView.findViewById(R.id.recipe_step_description_tv);
         stepDescriptionTV.setText(currentStep.getDescription());
@@ -98,10 +129,6 @@ public class RecipeStepDetailFragment extends Fragment {
                 mNextCallback.onNextStepSelected(currentStepId);
             }
         });
-        //exoplayer
-        mPlayerView = rootView.findViewById(R.id.recipe_step_exo_view);
-
-
 
         return rootView;
     }
@@ -109,7 +136,7 @@ public class RecipeStepDetailFragment extends Fragment {
 
     private void initMediaSession() {
         //create MediaSession
-        mMediaSession = new MediaSessionCompat(fragmentContext, TAG);
+        mMediaSession = new MediaSessionCompat(getContext(), TAG);
         //enable callbacks from buttons and controls
         mMediaSession.setFlags(
                 MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
@@ -130,12 +157,26 @@ public class RecipeStepDetailFragment extends Fragment {
 
         //Start media session
         mMediaSession.setActive(true);
-
     }
 
-    //init player url etc
-    private void initExoPlayer() {
-
+    //init player w/ url etc
+    private void initExoPlayer(Uri videoUrl, SimpleExoPlayerView exoView) {
+        //test for already established exo player
+        if (mExoPlayer == null) {
+            //create trackSelector
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            //create new instance of exo player with trackselector
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector);
+            //assign player to view
+            exoView.setPlayer(mExoPlayer);
+            //add listener for callbacks
+            mExoPlayer.addListener(this);
+            //add media source with video url
+            MediaSource mediaSource = new ExtractorMediaSource(videoUrl, new DefaultDataSourceFactory(
+                    getContext(), "BakingTime"), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
     }
 
     private class ExoCallBack extends MediaSessionCompat.Callback {
@@ -171,6 +212,61 @@ public class RecipeStepDetailFragment extends Fragment {
         bundle.putLong(EXO_POS_KEY, mExoPosition);
         bundle.putBoolean(EXO_FULLSCREEN_KEY, mExoFullscreen);
         bundle.putBoolean(EXO_STATE_KEY, mExoState);
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if ((playbackState == Player.STATE_READY) && playWhenReady) {
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PLAYING, mExoPlayer.getCurrentPosition(), 1f);
+        } else if (playbackState == Player.STATE_READY) {
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PAUSED, mExoPlayer.getCurrentPosition(), 1f);
+        }
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
+
     }
 
 }
